a567e4e5a2a2beb496b75a3f1bea00015d381332
JeroenReemer <jeroenreemer@gmail.com>
2021-01-18 14:25:22 +0100

    Comments

--- OOP_ijwit_hattum.py
+++ OOP_ijwit_hattum.py
@@ -1,9 +1,17 @@
+# imports
 import random
 from copy import deepcopy
 
 from Jeroen.classes.visualisation_OOP import Visualisation
 
 class Grid:
+    """
+    Grid initializes the grid space where the protein will be folded in.
+    Grid prints the grid as a list of lists.
+    Grid calculates the score of the folded protein.
+    Grid checks the possible folding moves for the protein.
+    Grid performs specific folding moves.
+    """
     def  __init__(self, size):
         actualgridsize = 2*size + 1
         self.content = [ [ (-1,'_') for i in range(actualgridsize) ] for j in range(actualgridsize) ]
@@ -12,6 +20,9 @@ class Grid:
         
 
     def printGrid(self):
+        """
+        Prints the grid as a list of lists.
+        """
         for row in self.content:
             print_list = []
             for tuble in row:
@@ -20,6 +31,10 @@ class Grid:
         print()
 
     def score(self):
+        """
+        Checks whether amino acids lay next to each other that can bond,
+        and returns the corresponding score.
+        """
         score = 0
         for x in range(len(self.content)-1):
             for y in range(len(self.content)-1):
@@ -29,10 +44,12 @@ class Grid:
                 if self.content[x][y][1] == self.content[x][y+1][1] == 'H':
                     if self.content[x][y][0] != self.content[x][y+1][0]+1 and self.content[x][y][0] != self.content[x][y+1][0]-1:
                         score = score-1
-
         return score
 
     def checkPossibleMoves(self):
+        """
+        Makes a list of all the possible folding moves for the next folding step.
+        """
         possiblemoves = []
         if self.totalMoves == 0:
             possiblemoves.append((self.currentPlace[0],self.currentPlace[1]))         
@@ -48,6 +65,9 @@ class Grid:
         return possiblemoves
 
     def performMove(self, move, letter):
+        """
+        Places the amino acids in the grid on their calculated place.
+        """
         self.content[move[0]][move[1]] = (self.totalMoves, letter)
         self.currentPlace = (move[0], move[1])
         self.totalMoves = self.totalMoves + 1
@@ -55,25 +75,45 @@ class Grid:
 
 
 class Protein:
+    """
+    Protein returns the subsequent letters
+    of the amino acid sequence one by one(queue).
+    """
     def __init__(self, code):
         self.code = code
         self.length = len(code)
 
     def pop_first_char(self):
+        """
+        Returns the first amino acid in the list.
+        """
         letter = self.code[0]
         self.code = self.code[1:]
         self.length = len(self.code)
         return letter
+
+
 ## HPHPPHHPHPPHPHHPPHPH
 moveList = []
+
+# initialize a Protein object called protein
 protein = Protein("HPHPPHHPHP")
+
+# initialize a Grid object called grid
 grid = Grid(protein.length)
+
+# append all the possible folding moves to the list moveList
 moveList.append(grid.checkPossibleMoves()[0])
+
+# places the amino acids in the grid on their calculated place
 grid.performMove(grid.checkPossibleMoves()[0], protein.pop_first_char())
+
+"""
 # while protein.length > 0:
 #     moves = grid.checkPossibleMoves()
 #     letter = protein.pop_first_char()
 #     grid.performMove(random.choice(moves),letter)
+"""
 
 
 
@@ -85,14 +125,19 @@ recursionAmount = 0
 
 
 def recursion(grid, protein, depth, firstMove):
-    """This is a recursive function
-    to find all possible folds"""
+    """
+    Recursion tries to find all possible foldings of 
+    the protein by looking a set steps(depth) ahead.
+    """
 
     global minimalScore, minimalGrid, minimalPerformedMove, recursionAmount
+
+    # print the amount of recursions for convenience purposes
     if (recursionAmount % 10000 )== 0:
         print(recursionAmount)
     recursionAmount = recursionAmount+1
 
+    # 
     if depth == 99 or protein.length == 0:
         #depth = 0
         S = grid.score()
@@ -105,10 +150,13 @@ def recursion(grid, protein, depth, firstMove):
             # grid.printGrid()
          
     else:
+
+        # check the possible moves
         moves = grid.checkPossibleMoves()
         
         for move in moves:
 
+            # try every possible move by using a deepcopy of the grid & protein
             gridCopy = deepcopy(grid)
             proteinCopy = deepcopy(protein)
             letter = proteinCopy.pop_first_char()

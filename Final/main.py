dd8614e716bf54eb3785db42c27948482e9f99a4
JeroenReemer <jeroenreemer@gmail.com>
2021-01-19 16:24:25 +0100

    Import van visualisation.py

--- Final/main.py
+++ Final/main.py
@@ -1,11 +1,21 @@
+#imports
 from classes_hattum import grid, protein
-
 from algorithms_hattum import greedy_lookahead
+from  visualisation import Visualisation
 
 
 
 if __name__ == "__main__":
-    protein = protein.Protein("HHPHHHPH")
+
+    # set the amino string you want to fold
+    amino_string = "HHPHHHPH"
+
+
+    protein = protein.Protein(amino_string)
     grid = grid.Grid(protein.length)
     print(type(grid))
     algo = greedy_lookahead.Greedy_lookahead(protein,grid,5)
+
+    # plots the folded protein in a grid
+    visualisation = Visualisation(amino_string, grid.score(), moveList)
+    visualisation.plot()

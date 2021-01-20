992f9964b61d41b6bbc75e58272ae72b0dc88af8
JeroenReemer <jeroenreemer@gmail.com>
2021-01-20 17:23:51 +0100

    Hattum werkt nu ook vor C'tjes

---
+++ Final/classes_hattum/grid_c.py
@@ -0,0 +1,78 @@
+class Grid_c:
+    """
+    Grid initializes the grid space where the protein will be folded in.
+    Grid prints the grid as a list of lists.
+    Grid calculates the score of the folded protein.
+    Grid checks the possible folding moves for the protein.
+    Grid performs specific folding moves.
+    """
+    def  __init__(self, size):
+        self.actualgridsize = 2*size + 1
+        self.content = [ [ (-1,'_') for i in range(self.actualgridsize) ] for j in range(self.actualgridsize) ]
+        self.currentPlace = (size,size)
+        self.totalMoves = 0
+        
+
+    def printGrid(self):
+        """
+        Prints the grid as a list of lists.
+        """
+        for row in self.content:
+            print_list = []
+            for tuble in row:
+                print_list.append(str(tuble[0])+tuble[1])
+            print(print_list)
+        print()
+
+    def score(self):
+        """
+        Checks whether amino acids lay next to each other that can bond,
+        and returns the corresponding score.
+        """
+        score = 0
+        for x in range(len(self.content)-1):
+            for y in range(len(self.content)-1):
+                if self.content[x][y][1] == self.content[x+1][y][1] == 'H':
+                    if self.content[x][y][0] != self.content[x+1][y][0]+1 and self.content[x][y][0] != self.content[x+1][y][0]-1:
+                            score = score-1
+                if self.content[x][y][1] == self.content[x][y+1][1] == 'H':
+                    if self.content[x][y][0] != self.content[x][y+1][0]+1 and self.content[x][y][0] != self.content[x][y+1][0]-1:
+                        score = score-1
+                if self.content[x][y][1] == self.content[x+1][y][1] == 'C':
+                    if self.content[x][y][0] != self.content[x+1][y][0]+1 and self.content[x][y][0] != self.content[x+1][y][0]-1:
+                            score = score-5
+                if self.content[x][y][1] == self.content[x][y+1][1] == 'C':
+                    if self.content[x][y][0] != self.content[x][y+1][0]+1 and self.content[x][y][0] != self.content[x][y+1][0]-1:
+                        score = score-5
+
+        return score
+
+    def checkPossibleMoves(self):
+        """
+        Makes a list of all the possible folding moves for the next folding step.
+        """
+        possiblemoves = []
+        if self.totalMoves == 0:
+            possiblemoves.append((self.currentPlace[0],self.currentPlace[1]))         
+        else:
+            if self.content[self.currentPlace[0]][self.currentPlace[1]+1][1] == '_':
+                possiblemoves.append((self.currentPlace[0],self.currentPlace[1]+1))
+            if self.content[self.currentPlace[0]][self.currentPlace[1]-1][1] == '_':
+                possiblemoves.append((self.currentPlace[0],self.currentPlace[1]-1))
+            if self.content[self.currentPlace[0]+1][self.currentPlace[1]][1] == '_':
+                possiblemoves.append((self.currentPlace[0]+1,self.currentPlace[1]))
+            if self.content[self.currentPlace[0]-1][self.currentPlace[1]][1] == '_':
+                possiblemoves.append((self.currentPlace[0]-1,self.currentPlace[1]))    
+        return possiblemoves
+
+    def performMove(self, move, letter):
+        """
+        Places the amino acids in the grid on their calculated place.
+        """
+        self.content[move[0]][move[1]] = (self.totalMoves, letter)
+        self.currentPlace = (move[0], move[1])
+        self.totalMoves = self.totalMoves + 1
+      
+    def clearGrid(self):
+        self.content = [ [ (-1,'_') for i in range(self.actualgridsize) ] for j in range(self.actualgridsize) ]
+        self.totalMoves = 0
--- Final/main.py
+++ Final/main.py
@@ -1,5 +1,5 @@
 #imports
-from classes_hattum import grid, protein
+from classes_hattum import grid_c, grid, protein
 from algorithms_hattum import greedy_lookahead
 from  visualisation import Visualisation
 
@@ -8,13 +8,28 @@ from  visualisation import Visualisation
 if __name__ == "__main__":
 
     # set the amino string you want to fold
-    amino_string = "HHPHHHPH"
+    #amino_string = "HHPHHHPHPHHHPH"
+    #amino_string = "HPHPPHHPHPPHPHHPPHPH"
+    #amino_string = "PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP"
+    #amino_string = "HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH"
+
+    amino_string = "PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP"
+    #amino_string = "CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC"
+    #amino_string = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
+    #amino_string = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
+
+    # set the number of steps to look ahead
+    steps = 5
 
 
     protein = protein.Protein(amino_string)
-    grid = grid.Grid(protein.length)
+
+    # choose to use grid.py or grid_c.py
+    #grid = grid.Grid(protein.length)
+    grid = grid_c.Grid_c(protein.length)
+
     print(type(grid))
-    algo = greedy_lookahead.Greedy_lookahead(protein,grid,5)
+    algo = greedy_lookahead.Greedy_lookahead(protein, grid, steps)
 
     # plots the folded protein in a grid
     visualisation = Visualisation(amino_string, grid.score(), algo.allMoves)
